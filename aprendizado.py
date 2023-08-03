8f5bd2d6c773db6ee955ad80255a57dc7aa9fc2d
Dali <82356573+OMateusSO@users.noreply.github.com>
2023-08-03 16:36:08 -0300

    Update aprendizado.py

--- aprendizado.py
+++ aprendizado.py
@@ -1,33 +1,33 @@
 #Test1
-from cgitb import text
+from cgitb import text;
 
 
-print("Olá Mundo!")
+print("Olá Mundo!");
 
 #Test2
-print(1)
-x = 2
-print (x)
+print(1);
+x = 2;
+print (x);
 
 #Test3
-x=1
-y=3
-print(x+y)
+x=1;
+y=3;
+print(x+y);
 
 #Test4
-text1 = 'oi'
-text2 = 'Python'
-print(text1 + text2)
+text1 = 'oi';
+text2 = 'Python';
+print(text1 + text2);
 
 #Test5
-text3 = 'oi'
-print(text3 * 3)
+text3 = 'oi';
+print(text3 * 3);
 
 #Test6
-print(text1.upper())
+print(text1.upper());
 
 #Test7
-print(text1.capitalize())
+print(text1.capitalize());
 
 
 
